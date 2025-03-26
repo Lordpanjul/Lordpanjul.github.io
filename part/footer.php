4f2ae7401262c3bc6e8427621d5d7ef1caa96b6b
PANJI KURNIAWAN <panjikurniawan777@gmail.com>
2025-03-27 03:16:53 +0700

    error

--- part/footer.php
+++
@@ -1,4 +0,0 @@
- <!-- Copyright Section-->
-        <div class="copyright py-4 text-center text-white">
-            <div class="container"><small>Copyright &copy; PANJI KURNIAWAN</small></div>
-        </div>
